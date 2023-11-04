b79eecce28ac4fe6eebe908dae8dd749e3aab639
karoljaro <karol_jaron@vp.pl>
2023-11-04 01:35:56 +0100

    pierdoły w nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      meta: [
+        { name: 'description', content: 'Transition Test.' },
+        { name: "robots", content: "noindex" },
+      ],
+      title: 'Transition',
+      htmlAttrs: {
+        lang: 'pl'
+      }
+    },
+  },
   devtools: { enabled: true },
   css: ['@/assets/scss/default.scss'],
   vite: {
